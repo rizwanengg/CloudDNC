8b7cc55e35fe82507044ca9e0b3c7af4faf6d697
Rizwan Shaikh <shaikh.rizwanengg@gmail.com>
2020-11-06 16:27:42 +0530

    GUI Modified by replacing MainScreen.ui to MainScreen.py

--- Launch.py
+++ Launch.py
@@ -1,36 +1,39 @@
 import _thread
 import sys
+from PyQt5 import QtWidgets
 
 import easygui
 import serial
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.uic import loadUi
-
-#from MachineFileRead import readFileFromMachine
+from MainScreen import Ui_DNCLoggerMainScreen
+#python -m PyQt5.uic.pyuic -x MainScreen.ui -o MainScreen.py
 from ReadJSON import *
 from SerialPortFunc import detectPort, openPort, selectPort
+
 try:
     ser = openPort(selectPort(), 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+    print("Read Port Open")
  #   _thread.start_new_thread(readFileFromMachine(), ser, ("RF"))
 
 except:
     print(IOError)
 
 
-class MainScreen(QMainWindow):
+class MainScreen(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainScreen, self).__init__()
-        self.initUI()
-        self.sendBtn.clicked.connect(self.on_sendBtn1_clicked)
-        self.pauseBtn.clicked.connect(self.on_pauseBtn1_clicked)
-        self.resumeBtn.clicked.connect(self.on_resumeBtn1_clicked)
-        self.abortBtn.clicked.connect(self.on_abortBtn1_clicked)
-        self.restartBtn.clicked.connect(self.on_restartBtn1_clicked)
-        self.backBtn.clicked.connect(self.on_backBtn1_clicked)
+        self.ui = Ui_DNCLoggerMainScreen()
+        self.ui.setupUi(self)
+        #self.ui.DNCLoggerMainScreen.resize(1024, 768)
+
+        self.ui.sendBtn.clicked.connect(self.on_sendBtn1_clicked)
+        self.ui.pauseBtn.clicked.connect(self.on_pauseBtn1_clicked)
+        self.ui.resumeBtn.clicked.connect(self.on_resumeBtn1_clicked)
+        self.ui.abortBtn.clicked.connect(self.on_abortBtn1_clicked)
+        self.ui.restartBtn.clicked.connect(self.on_restartBtn1_clicked)
+        self.ui.backBtn.clicked.connect(self.on_backBtn1_clicked)
         list_of_ports = detectPort()
         print(list_of_ports)
-    def initUI(self):
-        loadUi('MainScreen.ui', self)
+
 
     def on_sendBtn1_clicked(self):
         print("Send Btn Clicked")
@@ -99,12 +102,10 @@ class MainScreen(QMainWindow):
         print("back Btn Clicked")
 
     def addLineToFileDisplay(self,line):
-        self.fileDisplay.addItem(line)
+        self.ui.fileDisplay.addItem(line)
 
-def window():
-    app = QApplication(sys.argv)
-    w = MainScreen()
-    w.show()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = MainScreen()
+    window.show()
     sys.exit(app.exec_())
-#if __name__ == '__main__':
-window()
